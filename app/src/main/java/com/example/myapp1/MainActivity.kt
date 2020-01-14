5781cd3d30e97b315fdd80f7db2bfbef6dcd1487
Jouni Lammi <jjj.lammi@gmail.com>
2020-01-14 11:58:37 +0200

    Toast added
    Used https://www.tutorialkart.com/kotlin-android/set-onclicklistener-button-kotlin-android/ as an example to make a simple button with a toast message

--- app/src/main/java/com/example/myapp1/MainActivity.kt
+++ app/src/main/java/com/example/myapp1/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById(R.id.btn_click_me) as Button
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "Hello World!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
